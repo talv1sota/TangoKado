8cd8d355baa8f8f0726f965481fc43f07339eb73
talv1sota <269009753+talv1sota@users.noreply.github.com>
2026-03-18 14:27:28 -0400

    Add light/dark/system mode toggle, revert streak badge to simple style

--- TangoKado/ContentView.swift
+++ TangoKado/ContentView.swift
@@ -2,12 +2,24 @@ import SwiftUI
 
 struct ContentView: View {
     @AppStorage("hasSeenWelcome") private var hasSeenWelcome = false
+    @AppStorage("appColorScheme") private var appColorScheme = 0 // 0=system, 1=light, 2=dark
 
     var body: some View {
-        if hasSeenWelcome {
-            DeckListView()
-        } else {
-            WelcomeView(hasSeenWelcome: $hasSeenWelcome)
+        Group {
+            if hasSeenWelcome {
+                DeckListView()
+            } else {
+                WelcomeView(hasSeenWelcome: $hasSeenWelcome)
+            }
+        }
+        .preferredColorScheme(colorScheme)
+    }
+
+    private var colorScheme: ColorScheme? {
+        switch appColorScheme {
+        case 1: return .light
+        case 2: return .dark
+        default: return nil
         }
     }
 }
